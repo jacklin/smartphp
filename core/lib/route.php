f03c40aa767d7baa16c5340767f9e5d932d72916
超级手艺人 <fjxmlj007@163.com>
2017-09-10 23:13:45 +0800

    添加注释路由类使用方法说明

--- core/lib/route.php
+++ core/lib/route.php
@@ -2,6 +2,21 @@
 namespace core\lib;
 /**
 * 	路由类
+* 	所有的请求支持方法POST|GET|数据类型默认使用请求的数据类型，同时使用
+* 	GET使用方法如下:
+* 	Route::get('C/A',"RealC/RealA[string|function]");
+* 	Route::get('C/:id',"RealC/RealA[string|function]",'dataType'); //dataType[int|string|array]
+* 	
+* 	POST使用方法如下:
+* 	Route::post('C/A',"RealC/Real/A[string|function]");
+* 	Route::post('C/:id',"RealC/RealA[string|function]",'dataType'); //dataType[int|string|array]
+*
+*   任何模式请求方法如下:
+* 	Route::any('C/A',"RealC/RealA[string|function]");
+* 	Route::post('C/:id',"RealC/RealA",'dataType'); //dataType[int|string|array]
+*
+* 	
+* 	Route::miss('*',"RealC/RealA[string|function]");
 */
 class Route
 {
@@ -30,6 +45,8 @@ class Route
 						[$action, $param] = $expression;
 						if (strstr($param, ':')) {
 							self::$ruleMap[$realpath] = isset(self::$ruleMap[$realpath]) ? self::$ruleMap[$realpath] : $expression;
+						}else{
+
 						}
 						break;
 					
